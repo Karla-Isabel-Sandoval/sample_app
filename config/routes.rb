7c182c13acac62adc5c1a37597c6eba78f945299
Karla-Isabel-Sandoval <k.isabel.sandoval@gmail.com>
2017-04-20 19:35:18 -0700

    Add sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  resources :users
 end
 #using the get above arranges for route to respond to a GET request
 # For details on the DSL available within this file, see
