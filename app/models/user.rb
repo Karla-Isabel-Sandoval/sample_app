3475617c40dd3341dca66911f81118a3ccc389de
Karla-Isabel-Sandoval <k.isabel.sandoval@gmail.com>
2017-05-01 16:06:40 -0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,39 @@
 class User < ApplicationRecord
+
+  ### SCOPES
+
+  ### CONSTANTS
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  ### ATTR MACROS
+  has_secure_password
+
+  ### ASSOCIATIONS
+
+  ### VALIDATIONS
+  validates :email, presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  ### CALLBACKS
   before_save {
     #self.email = email.downcase
     email.downcase!
   }
-  validates :name, presence: true, length: {
-    maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :email, presence: true, length: {
-    maximum: 255 },
-    format: {
-      with: VALID_EMAIL_REGEX
-    },
-    uniqueness: {
-      case_sensitive: false
-    }
-    has_secure_password
-    validates :password,
-    presence: true, length: {
-      minimum: 6 }
-  end
+  ### OTHER MACROS
+  ### METHODS
+
+  # self METHODS
+  # public methods
+
+  protected
+  # protected methods
+
+  private
+  # private methods
+
+end
